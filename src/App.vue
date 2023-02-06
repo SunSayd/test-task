fcd7bef422090faf7b8c591bc1deacbbffce7624
vkakusha <gj580D$nCRPo>
2023-02-06 19:27:11 +0300

    test-task 2: base css layout, splitting main page into components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>
-    мое тестовое
-  </div>
+  <div class="container">
+      <Header/>
+      <div class="row">
+        <div
+            class="column-xxs-12"
+        >
+          <slider
+              style="height: 200px"
+          />
+        </div>
+        <product
+            style="height: 200px"
+            class="column-xxs-12 column-md-6 column-lg-4 column-xxl-3"
+        />
+        <product
+            style="height: 200px"
+            class="column-xxs-12 column-md-6 column-lg-4 column-xxl-3"
+        />
+        <product
+            style="height: 200px"
+            class="column-xxs-12 column-md-6 column-lg-4 column-xxl-3"
+        />
+        <product
+            style="height: 200px"
+            class="column-xxs-12 column-md-6 column-lg-4 column-xxl-3"
+        />
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
+import Header from "@/components/header.vue";
+import Slider from "@/components/slider.vue";
+import Product from "@/components/product.vue";
 export default {
   name: 'App',
-};
+  components: {Product, Slider, Header},
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
 </script>
---
+++ src/components/product.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div class="product"/>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Product"
+}
+</script>
+
+<style lang="scss">
+.product {
+  background: red;
+  border: 1px green solid;
+  height: 200px;
+}
+
+</style>
